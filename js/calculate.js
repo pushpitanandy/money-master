1a9b86ebac70d2a8e4670eb1dc6b1e1ba25658c5
pushpitanandy <nandypushpita@gmail.com>
2022-02-18 09:00:17 +0600

    strings and negative numbers handled in calculate button

--- js/calculate.js
+++ js/calculate.js
@@ -4,6 +4,20 @@ function getInputValue(inputId) {
     const userInput = parseFloat(inputField.value);
     return userInput;
 }
+
+//alert message for error
+function alert(placeId, message) {
+    //get the place to show message
+    let alertPlaceholder = document.getElementById(placeId);
+
+    //create a message 
+    var wrapper = document.createElement('div')
+    wrapper.innerHTML = '<div class="alert alert-warning alert-dismissible" role="alert">' + message + '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>'
+
+    //append to the place
+    alertPlaceholder.append(wrapper)
+}
+
 // calculate button event handler
 document.getElementById('calculate-button').addEventListener('click', function () {
 
@@ -13,13 +27,19 @@ document.getElementById('calculate-button').addEventListener('click', function (
     const rentExpense = getInputValue('rent-input');
     const clothesExpense = getInputValue('clothes-input');
 
-    //calculating total expense
-    const totalExpense = foodExpense + rentExpense + clothesExpense;
-    document.getElementById('total-expense').innerText = totalExpense;
+    //condition for checking strings and negative numbers in the input fields 
+    if ((userIncome > 0 && foodExpense > 0 && rentExpense > 0 && clothesExpense > 0) && (isNaN(userIncome) == false && isNaN(foodExpense) == false && isNaN(rentExpense) == false && isNaN(clothesExpense) == false)) {
+        //calculating total expense
+        const totalExpense = foodExpense + rentExpense + clothesExpense;
+        document.getElementById('total-expense').innerText = totalExpense;
 
-    //calculating balance
-    const balance = userIncome - totalExpense;
-    document.getElementById('balance').innerText = balance;
+        //calculating balance
+        const balance = userIncome - totalExpense;
+        document.getElementById('balance').innerText = balance;
+    }
+    else {
+        alert('calculate-btn-placeholder', 'Please provide a valid number');
+    }
 });
 
 //save button event handler
